65dfe01052174014a2b18f19fd45c1998e080092
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2013-06-13 12:42:01 +0200

    Testscript

---
+++ test.py
@@ -0,0 +1,22 @@
+import sys, re
+import csv
+from collections import defaultdict
+
+def openFile(filename):
+	return csv.reader(open(filename, "r"),delimiter='\t')
+
+
+
+tsv_file = openFile(sys.argv[1])
+column = int(sys.argv[2])
+occurences = defaultdict(int)
+
+for row in tsv_file:
+	key = row[column]
+	occurences[key] += 1
+
+for key in occurences:
+	print occurences[key], ',',  key
+
+
+
