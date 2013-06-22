d9133a7fc3a9dd6f99e16c8e8a8485d5804909e6
Maximilian Schneider <mail@maximilianschneider.net>
2013-06-23 01:53:16 +0200

    improved histogram

--- histogram.py
+++ histogram.py
@@ -6,24 +6,22 @@ def openFile(filename):
 
 
 
-tsv_file = openFile(sys.argv[1])
-column = int(sys.argv[2])
-occurences = defaultdict(int)
 
-for row in tsv_file:
-	key = row[column]
-	occurences[key] += 1
+for column in range(15)[1:]:
+	tsv_file = openFile(sys.argv[1])
+	occurences = defaultdict(int)
+	for row in tsv_file:
+		key = row[column]
+		occurences[key] += 1
 
-n = sum(occurences.values())
-entropy = 0.0
-for key in occurences:
-    p = 1.0 * occurences[key] / n
-    entropy -= p * math.log(p)
+	distinct_values = len(occurences.keys())
+	print column, "distinct:", distinct_values
 
-print "entropy:", entropy
-
-for key in occurences:
-	#print str(occurences[key]) + "," + key + "," + str(len(key))
-    pass
+	entropy = 0.0
+	n = sum(occurences.values())
+	for key in occurences:
+		p = 1.0 * occurences[key] / n
+		entropy -= p * math.log(p)
+	print column, "entropy:", entropy
 
 
